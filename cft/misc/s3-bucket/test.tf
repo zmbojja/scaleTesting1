7af34794a712d178a647d319a51efd1496544bb0
mallik-user1 <42467608+mallik-user1@users.noreply.github.com>
2021-10-28 11:45:49 +0530

    Update test.tf

--- cft/misc/s3-bucket/test.tf
+++ cft/misc/s3-bucket/test.tf
@@ -16,6 +16,11 @@ resource "aws_s3_bucket" "mybucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
+  #zs:skip=AC_AWS_0214 ignore
+  #zs:skip=AC_AWS_0497 ignore
+  #zs:skip=AC_AWS_0208 ignore
+  #zs:skip=AC_AWS_0496 ignore
+  #zs:skip=AC_AWS_0214 ignore
   bucket = aws_s3_bucket.example.id
 
   block_public_acls   = true
